a8770a383fd62b5b5bc5171998e243549f5b6297
jeromeku <jerome.ku@gmail.com>
2025-05-31 09:30:27 -0700

    add generator op and kernel checks

--- python/cutlass_library/run_generator_manual.sh
+++ python/cutlass_library/run_generator_manual.sh
@@ -4,22 +4,25 @@ set -euo pipefail
 #DEBUG_GENERATOR: Namespace(operations='all', build_dir='/home/jeromeku/kernels/cfx-article-src/external/cutlass/profiler_test_build', curr_build_dir='/home/jeromeku/kernels/cfx-article-src/external/cutlass/profiler_test_build/tools/library', generator_target='library', architectures='90a', kernels='cutlass3x_sm90_tensorop_gemm_f16_f16_f32_f16_f16*_tn*', ignore_kernels='', exclude_kernels='', filter_by_cc='True', cuda_version='12.8.61', kernel_filter_file='', selected_kernel_list='/home/jeromeku/kernels/cfx-article-src/external/cutlass/profiler_test_build/tools/library/generated_kernels.txt', interface_dir=None, disable_full_archs_compilation=False, log_level=10, instantiation_level='0000', disable_cutlass_package_imports=True)
 
 OPERATIONS='gemm'
-BUILD_DIR='./generator_test'
+INSTANTIATION_LEVEL='0000'
+ARCHITECTURES='90a'
+TYPES="f16_f16_f32_void_f16"
+KERNELS="cutlass3x_sm90_tensorop_gemm_${TYPES}*"
+# KERNELS="${KERNELS},*_spgemm_*"
+
+BUILD_DIR="./generated/${OPERATIONS}_${ARCHITECTURES}/${TYPES}/${INSTANTIATION_LEVEL}"
 CURR_BUILD_DIR="${BUILD_DIR}/library"
 
 mkdir -p ${CURR_BUILD_DIR}
 
 GENERATOR_TARGET='library'
-ARCHITECTURES='90a'
-KERNELS='cutlass3x_sm90_tensorop_gemm_f16_f16_f32_f16_f16*_tn*'
 #ignore_kernels='', exclude_kernels='', 
 FILTER_BY_CC='True'
 CUDA_VERSION='12.8.61'
 #kernel_filter_file='', 
 SELECTED_KERNEL_LIST="${BUILD_DIR}/generated_kernels.txt"
 #, interface_dir=None, disable_full_archs_compilation=False, 
-LOG_LEVEL=DEBUG
-INSTANTIATION_LEVEL='0000'
+LOG_LEVEL=INFO
 
 CMD="python generator.py \
     --operations=${OPERATIONS} \
@@ -38,4 +41,4 @@ CMD="python generator.py \
 CMD="PYTHONPATH=`pwd` ${CMD}" 
 
 echo $CMD
-eval $CMD
+eval $CMD 2>&1 | tee ${BUILD_DIR}/gen.log
