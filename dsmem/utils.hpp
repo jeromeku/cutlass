130bebb037a0720db6318676b16291981e1425a3
jeromeku <jerome.ku@gmail.com>
2025-08-30 01:16:55 +0000

    formatting

--- dsmem/utils.hpp
+++ dsmem/utils.hpp
@@ -5,45 +5,46 @@
 #define PRINT_CLUSTER_DELAY_CYCLES 20000000ULL
 #endif
 
-#define PRINT_CLUSTER(expr)                                                                              \
-  do                                                                                                     \
-  {                                                                                                      \
-    if (threadIdx.x == 0 && threadIdx.y == 0)                                                            \
-    {                                                                                                    \
-      int _cluster_rank = cute::block_rank_in_cluster();                                                 \
-      unsigned long long _delay_cycles = PRINT_CLUSTER_DELAY_CYCLES * (unsigned long long)_cluster_rank; \
-      if (_delay_cycles)                                                                                 \
-      {                                                                                                  \
-        unsigned long long _start = clock64();                                                           \
-        while (clock64() - _start < _delay_cycles)                                                       \
-        { /* spin */                                                                                     \
-        }                                                                                                \
-      }                                                                                                  \
-      printf("ClusterRank[%d]: ", _cluster_rank);                                                        \
-      expr;                                                                                              \
-      printf("\n");                                                                                      \
-    }                                                                                                    \
-  } while (0)
+// Print msg prefixed with cluster rank
+// Sets delay based on cluster rank to prevent interleaved printing
+#define PRINT_CLUSTER(expr)                                                                                    \
+    do                                                                                                         \
+    {                                                                                                          \
+        if (threadIdx.x == 0 && threadIdx.y == 0)                                                              \
+        {                                                                                                      \
+            int _cluster_rank = cute::block_rank_in_cluster();                                                 \
+            unsigned long long _delay_cycles = PRINT_CLUSTER_DELAY_CYCLES * (unsigned long long)_cluster_rank; \
+            if (_delay_cycles)                                                                                 \
+            {                                                                                                  \
+                unsigned long long _start = clock64();                                                         \
+                while (clock64() - _start < _delay_cycles)                                                     \
+                { /* spin */                                                                                   \
+                }                                                                                              \
+            }                                                                                                  \
+            printf("ClusterRank[%d]: ", _cluster_rank);                                                        \
+            expr;                                                                                              \
+            printf("\n");                                                                                      \
+        }                                                                                                      \
+    } while (0)
 
-#define CUDA_CHECK(call)                                            \
-  do                                                                \
-  {                                                                 \
-    cudaError_t error = call;                                       \
-    if (error != cudaSuccess)                                       \
-    {                                                               \
-      std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__  \
-                << " - " << cudaGetErrorString(error) << std::endl; \
-      std::exit(EXIT_FAILURE);                                      \
-    }                                                               \
-  } while (0)
+#define CUDA_CHECK(call)                                                  \
+    do                                                                    \
+    {                                                                     \
+        cudaError_t error = call;                                         \
+        if (error != cudaSuccess)                                         \
+        {                                                                 \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__  \
+                      << " - " << cudaGetErrorString(error) << std::endl; \
+            std::exit(EXIT_FAILURE);                                      \
+        }                                                                 \
+    } while (0)
 
 // Function to check and print last CUDA error
 void checkLastCudaError(const char *msg = "CUDA Error")
 {
-  cudaError_t error = cudaGetLastError();
-  if (error != cudaSuccess)
-  {
-    std::cerr << msg << ": " << cudaGetErrorString(error) << std::endl;
-  }
-
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess)
+    {
+        std::cerr << msg << ": " << cudaGetErrorString(error) << std::endl;
+    }
 }
