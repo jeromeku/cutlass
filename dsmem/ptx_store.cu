7534687efc9c2ce56ce78f6071fafa01394b385f
jeromeku <jerome.ku@gmail.com>
2025-08-30 01:06:13 +0000

    refactor utils

--- dsmem/ptx_store.cu
+++ dsmem/ptx_store.cu
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cute/tensor.hpp>
 #include <cutlass/cluster_launch.hpp>
+#include "utils.hpp"
 
 using namespace cute;
 
@@ -13,51 +14,6 @@ constexpr int NUM_ELEMS = 2;
 constexpr int NUM_THREADS = 32;
 constexpr int CLUSTER_SIZE = 4;
 
-#ifndef PRINT_CLUSTER_DELAY_CYCLES
-#define PRINT_CLUSTER_DELAY_CYCLES 20000000ULL
-#endif
-
-#define PRINT_CLUSTER(expr)                                                                              \
-  do                                                                                                     \
-  {                                                                                                      \
-    if (threadIdx.x == 0 && threadIdx.y == 0)                                                            \
-    {                                                                                                    \
-      int _cluster_rank = cute::block_rank_in_cluster();                                                 \
-      unsigned long long _delay_cycles = PRINT_CLUSTER_DELAY_CYCLES * (unsigned long long)_cluster_rank; \
-      if (_delay_cycles)                                                                                 \
-      {                                                                                                  \
-        unsigned long long _start = clock64();                                                           \
-        while (clock64() - _start < _delay_cycles)                                                       \
-        { /* spin */                                                                                     \
-        }                                                                                                \
-      }                                                                                                  \
-      printf("ClusterRank[%d]: ", _cluster_rank);                                                        \
-      expr;                                                                                              \
-      printf("\n");                                                                                      \
-    }                                                                                                    \
-  } while (0)
-
-#define CUDA_CHECK(call)                                            \
-  do                                                                \
-  {                                                                 \
-    cudaError_t error = call;                                       \
-    if (error != cudaSuccess)                                       \
-    {                                                               \
-      std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__  \
-                << " - " << cudaGetErrorString(error) << std::endl; \
-      std::exit(EXIT_FAILURE);                                      \
-    }                                                               \
-  } while (0)
-
-// Function to check and print last CUDA error
-void checkLastCudaError(const char *msg = "CUDA Error")
-{
-  cudaError_t error = cudaGetLastError();
-  if (error != cudaSuccess)
-  {
-    std::cerr << msg << ": " << cudaGetErrorString(error) << std::endl;
-  }
-}
 
 // Store value to remote shared memory in the cluster
 CUTE_DEVICE
