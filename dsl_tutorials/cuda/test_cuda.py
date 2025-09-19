a215c32e011f87218c4819b26ff04d110ef7788c
jeromeku <jerome.ku@gmail.com>
2025-09-19 19:38:17 +0000

    working cuModule

--- dsl_tutorials/cuda/test_cuda.py
+++ dsl_tutorials/cuda/test_cuda.py
@@ -1,7 +1,8 @@
 import cuda.bindings.driver as cuda
 from cuda.core.experimental._utils.cuda_utils import handle_return
-
+import numpy as np
 DEVICE = 0
+driver = cuda
 
 # Start conservative; bump later once it works.
 kPTX = r"""
@@ -40,7 +41,25 @@ DONE:
     ret;
 }
 """
+def _cudaGetErrorEnum(error):
+    if isinstance(error, driver.CUresult):
+        err, name = driver.cuGetErrorName(error)
+        return name if err == driver.CUresult.CUDA_SUCCESS else "<unknown>"
+    # elif isinstance(error, nvrtc.nvrtcResult):
+    #     return nvrtc.nvrtcGetErrorString(error)[1]
+    else:
+        raise RuntimeError('Unknown error type: {}'.format(error))
 
+def checkCudaErrors(result):
+    if result[0].value:
+        raise RuntimeError("CUDA error code={}({})".format(result[0].value, _cudaGetErrorEnum(result[0])))
+    if len(result) == 1:
+        return None
+    elif len(result) == 2:
+        return result[1]
+    else:
+        return result[1:]
+    
 def test_cuda_ptx():
     handle_return(cuda.cuInit(0))
     _err, dev = cuda.cuDeviceGet(DEVICE)
@@ -88,4 +107,37 @@ def test_cuda_ptx():
         raise RuntimeError(f"cuModuleLoadDataEx failed: {res}")
 
     print("Module loaded:", mod)
+
+    func = handle_return(cuda.cuModuleGetFunction(mod, b"add1"))
+    num_elements = 16
+    N = np.array(num_elements, dtype=np.uint32)
+    hX = np.ones(N, dtype=np.float32)
+    BUFFER_SIZE = N * hX.itemsize
+    assert BUFFER_SIZE == hX.nbytes
+
+    _dX = handle_return(cuda.cuMemAlloc(hX.nbytes))
+    checkCudaErrors(cuda.cuMemcpyHtoD(_dX, hX.ctypes.data, hX.nbytes))
+    dX = np.array([int(_dX)], dtype=np.int64)
+    # kernel params are pointers to host values
+    kernel_params = (dX, N)
+    kernel_params = np.array([arg.ctypes.data for arg in kernel_params], dtype=np.uint64)
+
+    # cuda-python wants a tuple of pointers; Helper marshals them
+    checkCudaErrors(cuda.cuLaunchKernel(
+        func,
+        1, 1, 1,          # grid
+        int(N), 1, 1,     # block (one thread per elem for demo)
+        0, #sharedmem 
+        0, # stream
+        kernel_params.ctypes.data,    # kernel params
+        0
+    ))
+    checkCudaErrors(cuda.cuCtxSynchronize())
+
+    out = np.empty_like(hX)
+    checkCudaErrors(cuda.cuMemcpyDtoH(out.ctypes.data, dX, out.nbytes))
+    print("out:", out[:8])
+
+    checkCudaErrors(cuda.cuMemFree(dX))
+
 test_cuda_ptx()
